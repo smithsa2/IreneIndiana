d6ed180721cc096c9f54fbcd1d16ced18d66d66d
smithsa2 <99689464+smithsa2@users.noreply.github.com>
2022-06-23 10:28:38 +1200

    Basic Updates

--- game_menu_test.py
+++ game_menu_test.py
@@ -1,2 +1,18 @@
 ##
 # game_menu_test.py
+# 23/06/2022
+# SamuelSmith
+# Test of main menu for IreneIndiana
+
+game_menu = True
+
+while game_menu:
+    print("""   - Irene Indiana -   
+======================
+
+1) New Game
+2) Continue Game
+3) Credits
+4) Help & Tutorials
+5) Quit""")
+    
