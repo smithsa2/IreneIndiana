1a2e0f5d9a5f2e583fec5cd8f7a7b422a82f4804
smithsa2 <samuel.smith@student.onslow.school.nz>
2022-06-23 10:40:47 +1200

    Update 2

--- game_menu_test.py
+++ game_menu_test.py
@@ -4,10 +4,13 @@
 # SamuelSmith
 # Test of main menu for IreneIndiana
 
+import time
+
 game_menu = True
 
 while game_menu:
-    print("""   - Irene Indiana -   
+    print("""
+   - Irene Indiana -   
 ======================
 
 1) New Game
@@ -15,4 +18,20 @@ while game_menu:
 3) Credits
 4) Help & Tutorials
 5) Quit""")
+    user_input = input("> ").strip().lower()
+    if user_input == '1':
+        pass
+    elif user_input == '2':
+        pass
+    elif user_input == '3':
+        pass
+    elif user_input == '4':
+        pass
+    elif user_input == '5':
+        game_menu = False
+    else:
+        print("That wasn't an option")
+    time.sleep(0.1)
     
+print("\nThanks for playing Irene Indiana!")
+time.sleep(0.1)
