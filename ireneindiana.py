f06b80d31ba5e537a3ef3372bba8634697201bda
smithsa2 <99689464+smithsa2@users.noreply.github.com>
2022-06-30 10:45:14 +1200

    Added Room descriptions and improved structure

--- ireneindiana.py
+++ ireneindiana.py
@@ -4,6 +4,10 @@
 # Samuel Smith
 # The computer game "Where in onslow college is Irene Indiana?"
 
+import os
+
+#Funtions
+
 def main_menu():
     global game_loop, game_state
     print("""
@@ -17,13 +21,16 @@ def main_menu():
 5) Quit""")
     user_input = input("> ").strip().lower() 
     if user_input == '1':
-        print("Error: feature not added yet")
+        new_game()
     elif user_input == '2':
-        print("No current game")
+        if not os.path.exists("/game_data/current_game.txt"):
+            print("No current game")
+        else:
+            load_game()
     elif user_input == '3':
-        print("Made by me")
+        print(CREDITS)
     elif user_input == '4':
-        print(credits)
+        print(HELP)
     elif user_input == '5':
         game_loop = False
     else:
@@ -31,15 +38,62 @@ def main_menu():
 
 
 def new_game():
-    # Figure out how and where I want to store the game data
-    # Do Last
-    pass
+    global game_state
+    # Figure out how and where I want to store the game data (do last)
+    game_state = True
+
+def load_game():
+    with open("/game_data/current_game.txt", 'r'):
+        pass
+
+def action_menu():
+    print("""""")
+
+# Constants
+CREDITS = "Made by me"
+HELP = "Google it"
+MAP = [['Top field', 'Chemistry classrooms', 'Science hallway',
+          'Maths corridor', 'Student carpark'],
+         ['Old Skatebowl', 'Physics classrooms', 'Garden',
+          'Digital Tech classes', 'Art department'],
+         ['English block', 'Library', 'Social Sciences',
+          'Accounting & Business', 'Hall'],
+         ['Rear driveway', 'Student Centre', 'Quad', 'Main Driveway',
+          'Design Tech'],
+         ['Dairy', 'Table mountain: (main)', 'Table mountain (far)',
+          'Gym','Bottom field']]
+
+DESCRIPTIONS = []
+
+if not os.path.exists("game_data/descriptions.txt"):
+    print("Descriptions file is missing. Cannot Run")
+    quit()
+else:
+    with open("game_data/descriptions.txt", 'r') as file:
+        x = file.readlines()
+        for i in range(5):
+            DESCRIPTIONS.append([])
+            for j in range(5):
+                DESCRIPTIONS[i].append(x[i*5+j])
+
+POSSIBLE_ACTIONS = [[],
+    [],
+    [],
+    [],
+    []]
+
+# Variables
+pos = [4,0]
 
 
-game_state = main_menu
+game_state = False
 game_loop = True
 
 while game_loop:
-    game_state()
+    if not game_state:
+        main_menu()
+    else:
+        action_menu()
 
-print("\nThanks for playing Irene Indiana!")
+
+print("\nThanks for playing 'Where in Onslow College is Irene Indiana?'")
