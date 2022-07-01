0e20eb139a15400191bd669fa22553baee8a6a9d
smithsa2 <99689464+smithsa2@users.noreply.github.com>
2022-07-01 13:24:12 +1200

    Update

--- ireneindiana.py
+++ ireneindiana.py
@@ -6,27 +6,28 @@
 
 import os
 
-#Funtions
+# Funtions
+
 
 def main_menu():
     global game_loop, game_state
     print("""
-   - Irene Indiana -   
+   - Irene Indiana -
 ======================
 
 1) New Game
-2) Continue Game
+2) Stats
 3) Credits
 4) Help & Tutorials
 5) Quit""")
-    user_input = input("> ").strip().lower() 
+    user_input = input("> ").strip().lower()
     if user_input == '1':
         new_game()
     elif user_input == '2':
         if not os.path.exists("game_data/current_game.txt"):
-            print("No current game")
+            print("No stats")
         else:
-            load_game()
+            print_stats()
     elif user_input == '3':
         print(CREDITS)
     elif user_input == '4':
@@ -39,29 +40,48 @@ def main_menu():
 
 def new_game():
     global game_state
-    # Figure out how and where I want to store the game data (do last)
     game_state = True
 
-def load_game():
-    with open("game_data/current_game.txt", 'r'):
-        pass
+
+
+def print_stats():
+    pass
+
 
 def action_menu():
     print("""""")
 
+
+def pick_up(key):
+    pass
+
+
+def check_irene(key):
+    pass
+
+
+def talk(key):
+    pass
+
 # Constants
-CREDITS = "Made by me"
+CREDITS = """
+
+===========================
+    Made by Samuel Smith
+===========================
+
+"""
 HELP = "Google it"
 MAP = [['Top field', 'Chemistry classrooms', 'Science hallway',
-          'Maths corridor', 'Student carpark'],
-         ['Old Skatebowl', 'Physics classrooms', 'Garden',
-          'Digital Tech classes', 'Art department'],
-         ['English block', 'Library', 'Social Sciences',
-          'Accounting & Business', 'Hall'],
-         ['Rear driveway', 'Student Centre', 'Quad', 'Main Driveway',
-          'Design Tech'],
-         ['Dairy', 'Table mountain: (main)', 'Table mountain (far)',
-          'Gym','Bottom field']]
+        'Maths corridor', 'Student carpark'],
+       ['Old Skatebowl', 'Physics classrooms', 'Garden',
+        'Digital Tech classes', 'Art department'],
+       ['English block', 'Library', 'Social Sciences',
+        'Accounting & Business', 'Hall'],
+       ['Rear driveway', 'Student Centre', 'Quad', 'Main Driveway',
+        'Design Tech'],
+       ['Dairy', 'Table mountain: (main)', 'Table mountain (far)',
+        'Gym', 'Bottom field']]
 
 DESCRIPTIONS = []
 
@@ -75,7 +95,8 @@ else:
             DESCRIPTIONS.append([])
             for j in range(5):
                 DESCRIPTIONS[i].append(x[i*5+j])
-#Format ['Action name', 'name of speaker', [dialogue1, dialouge2, etc.]]
+
+# Format ['Action name', 'name of speaker', [dialogue1, dialouge2, etc.]]
 POSSIBLE_SPEAK = [[
     [["Yell to Runners", "Runner: ", ["I ... heard ... {} ... was ... near ... {} ...", ""]]],
     [],
@@ -83,137 +104,73 @@ POSSIBLE_SPEAK = [[
     [],
     []],
     [[],
-    [],
-    [],
-    [],
-    []],
+     [],
+     [],
+     [],
+     []],
     [[],
-    [],
-    [],
-    [],
-    []],
+     [],
+     [],
+     [],
+     []],
     [[],
-    [],
-    [],
-    [],
-    []],
+     [],
+     [],
+     [],
+     []],
     [[],
-    [],
-    [],
-    [],
-    []]]
+     [],
+     [],
+     [],
+     []]]
 
-POSSIBLE_NON_CLUE = [[
-    [],
-    [],
-    [],
-    [],
-    []],
+POSSIBLE_NON_CLUE = [
     [[],
-    [],
-    [],
-    [],
-    []],
+     [],
+     [],
+     [],
+     []],
     [[],
-    [],
-    [],
-    [],
-    []],
+     [],
+     [],
+     [],
+     []],
     [[],
-    [],
-    [],
-    [],
-    []],
+     [],
+     [],
+     [],
+     []],
     [[],
-    [],
-    [],
-    [],
-    []]]
+     [],
+     [],
+     [],
+     []],
+    [[],
+     [],
+     [],
+     [],
+     []]]
 
-POSSIBLE_CONTAINER = [[
-    [["Open Shipping Container (requires a means of opening a lock)", ]],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []]]
+# [['container desc', 'open attempt', 'open', [locx, locy], reward_funct],
+# ['key desc', 'pick up key', [locx, locy]]]
+POSSIBLE_CONTAINER = [
+    [["Open Shipping Container", "Cannot open, the old rusty lock wont budge",
+      "The acid dissolved the rusty latch and the door swings open", [0, 0], check_irene],
+     ["Acid (could be used to dissolve metal or remove rust)", "Picked up acid (don't spill it)", [1, 0]]],
+    [["Talk to hooded kid", "He ignores you, entirely obsessed with the comforting glow of his smartphone",
+      "The smell of the lollies, tantalisingly sweet and delicous, fills the air. The hooded kid notices and glances up, \
+      entranced by the aroma. You see longing in his eyes, to break free, to escape the emotionally manipulating programs.\
+      He starts towards you, but shudders and gives in. He returns to the glowing rectangle, encompassed in its friendly\
+      warmth.", [0,1], pick_up], ["A dollar bag of lollies (could be used to bribe children)", "You paid for the dollar bag and put it in your pocket", [0,4]]],
+    [["A battered silver car, parked on a horrific angle, sits at the edge of the carpark. Could it contain a secret?", "You try the boot, but it's locked.",
+      "You open the boot and find a note", pick_up],["The car keys have the same logo as the one in the carpark", 
+      "You picked up the car keys and quietly slid them into your pocket", [4,1]]]]
 
-POSSIBLE_KEY = [[
-    [],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []]]
-
-POSSIBLE_CIPHER = [[
-    [],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []],
-    [[],
-    [],
-    [],
-    [],
-    []]]
+POSSIBLE_CIPHER = []
 
 
 # Variables
-pos = [4,0]
+pos = [4, 0]
 
 game_state = False
 game_loop = True
